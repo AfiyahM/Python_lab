f510648779a8e2f3158be7127d35b78156180f83
AfiyahM <afu2974@gmail.com>
2024-10-21 15:53:52 +0530

    numpy

--- NUMPY/ALL.py
+++ NUMPY/ALL.py
@@ -52,3 +52,12 @@ standard_deviation=np.std(a1)
 min=np.min(a1)
 max=np.max(a1)
 
+
+import numpy as np
+
+
+arr = np.array([[[[1, 2], [3, 4]], [[5, 6], [7, 8]]], 
+                 [[[9, 10], [11, 12]], [[13, 14], [15, 16]]]])
+
+print(arr)
+print(arr.shape)
